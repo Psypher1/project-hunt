97d1958868424a862d8ed6bea099786fec72dda1
Dante <jamesmidzi@gmail.com>
2023-10-05 21:05:44 +0200

    started modal stuff

--- web/src/routes/my/account/+page.svelte
+++ web/src/routes/my/account/+page.svelte
@@ -1,10 +1,17 @@
 <script>
-	export let data;
 	import { Input } from "$ui";
+
+	export let data;
+	let loading;
+	$: loading = false;
 </script>
 
 <h2 class="text-lg font-semibold">Your Account</h2>
 
-<form action="?/updateUsername" method="post" class="mt-8">
+<form class="mt-8">
 	<Input name="username" type="text" label="Username" value={data?.user?.username} disabled />
+	<button
+		class="w-full py-2 px-4 border bg-blue-700 rounded text-blue-50 hover:bg-blue-600 transition duration-300"
+		>Change Username</button
+	>
 </form>
