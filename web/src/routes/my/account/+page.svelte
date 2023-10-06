27cb417b5005c75ad636729fd4d8f7dee9bdd2fc
Dante <jamesmidzi@gmail.com>
2023-10-06 17:50:48 +0200

    progressive enhance of account page

--- web/src/routes/my/account/+page.svelte
+++ web/src/routes/my/account/+page.svelte
@@ -1,17 +1,39 @@
 <script>
+	import { enhance, applyAction } from "$app/forms";
+	import { invalidateAll } from "$app/navigation";
+
 	import { Input } from "$ui";
+	import { Modal } from "$features";
 
 	export let data;
 	// export let form;
 	let loading;
 	$: loading = false;
 
-	import { Modal } from "$features";
-
-	let showModal = false;
+	let showModal;
+	$: showModal = false;
 
 	function handleToggleModal() {
 		showModal = !showModal;
+	}
+
+	function submitUpdatUsername() {
+		loading = true;
+		showModal = true;
+
+		return async ({ result }) => {
+			switch (result.type) {
+				case "success":
+					await invalidateAll();
+					showModal = false;
+					break;
+				case "error":
+					break;
+				default:
+					await applyAction(result);
+			}
+			loading = false;
+		};
 	}
 </script>
 
@@ -28,16 +50,22 @@
 
 	<Modal title="Username Change" open={showModal} on:close={handleToggleModal}>
 		<svelte:fragment slot="body">
-			<form action="?/updateUsername" method="post" class=" mx-auto">
+			<form
+				action="?/updateUsername"
+				method="post"
+				use:enhance={submitUpdatUsername}
+				class=" mx-auto"
+			>
 				<Input
 					name="username"
 					label="Enter New Username"
 					type="text"
 					value={data?.user?.username}
+					disabled={loading}
 				/>
 				<button
 					class="w-full py-2 px-4 border bg-blue-700 rounded text-blue-50 hover:bg-blue-600 transition duration-300"
-					>Change Username</button
+					disabled={loading}>{loading ? "Loading..." : "Change Username"}</button
 				>
 			</form>
 		</svelte:fragment>
