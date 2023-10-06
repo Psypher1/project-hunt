3eed855410ecb5d8a03cb1170253a63e960c0428
Dante <jamesmidzi@gmail.com>
2023-10-06 20:28:48 +0200

    textarea

--- web/src/ui/Textarea.svelte
+++ web/src/ui/Textarea.svelte
@@ -5,13 +5,13 @@
 	export let placeholder = "";
 </script>
 
-<div class="">
+<div class="mb-2">
 	<label for={name} class="block uppercase font-bold text-gray-600 text-xs">{label}</label>
 	<textarea
 		{name}
 		{value}
 		{placeholder}
-		class="mt-1 border border-gray-400"
+		class={`mt-1 w-full block p-2 border border-gray-400 placeholder:text-gray-400`}
 		id=""
 		cols="30"
 		rows="5"
