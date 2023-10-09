78e21d5db423ae953b59118a8274e8fd212975fe
Dante <jamesmidzi@gmail.com>
2023-10-09 12:48:06 +0200

    created my projects route and implemented deleting of project

---
+++ web/src/routes/my/projects/+page@.svelte
@@ -0,0 +1,36 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+
+	import { getImageUrl } from "$lib/helpers";
+	import { ProjectCard } from "$features";
+
+	// console.log(data);
+
+	const { projects } = data;
+</script>
+
+<svelte:head>
+	<title>My Projects</title>
+</svelte:head>
+
+<div class="py-5">
+	<h1 class="text-3xl font-bold">My Projects</h1>
+</div>
+
+<section class="">
+	{#if projects.length === 0}
+		<div class="text-center">
+			<h2 class="text-xl mb-2">It seems that you don't have any projects</h2>
+			<a
+				href="/projects/new"
+				class="py-2 px-4 block border bg-blue-700 rounded text-blue-50 hover:bg-blue-600 transition duration-300 w-fit mx-auto"
+				>Add one</a
+			>
+		</div>
+	{:else}
+		{#each projects as project}
+			<ProjectCard {project} />
+		{/each}
+	{/if}
+</section>
