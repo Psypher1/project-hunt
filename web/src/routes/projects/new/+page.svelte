739ef84d8c5e67e91ddcfe955b5601b506f6a526
Dante <jamesmidzi@gmail.com>
2023-10-09 20:28:22 +0200

    ehnaced new project form

--- web/src/routes/projects/new/+page.svelte
+++ web/src/routes/projects/new/+page.svelte
@@ -1,10 +1,27 @@
 <script>
-	/** @type {import('./$types').PageData} */
-	// export let data;
+	import { enhance, applyAction } from "$app/forms";
+	import { invalidateAll } from "$app/navigation";
 
-	import { HeadingOne, HeadingTwo } from "$ui/headings";
 	import { Input, Textarea } from "$ui";
-	import { tick } from "svelte";
+
+	let loading;
+	$: loading = false;
+
+	function addProjectHandler() {
+		loading = true;
+		return async ({ result }) => {
+			switch (result.type) {
+				case "success":
+					await invalidateAll();
+					break;
+				case "error":
+					break;
+				default:
+					await applyAction(result);
+			}
+			loading = false;
+		};
+	}
 </script>
 
 <svelte:head>
@@ -16,26 +33,48 @@
 	<p>Tell us about the cool thing you made.</p>
 </div>
 
-<form action="?/create" method="POST" enctype="multipart/form-data" class="max-w-xl mx-auto">
-	<Input name="title" type="text" label="Project Title" placeholder="Project Title" required />
+<form
+	action="?/create"
+	method="POST"
+	use:enhance={addProjectHandler}
+	enctype="multipart/form-data"
+	class="max-w-xl mx-auto"
+>
+	<Input
+		name="title"
+		type="text"
+		label="Project Title"
+		placeholder="Project Title"
+		required
+		disabled={loading}
+	/>
 	<Input
 		name="tagline"
 		type="text"
 		label="Project Tagline"
 		placeholder="Project Tagline"
 		required
+		disabled={loading}
 	/>
-	<Input name="link" type="text" label="Project Link" placeholder="Project Link" required />
+	<Input
+		name="link"
+		type="text"
+		label="Project Link"
+		placeholder="Project Link"
+		required
+		disabled={loading}
+	/>
 	<Textarea
 		name="description"
 		type="text"
 		label="Project Description"
 		placeholder="Project Description"
+		disabled={loading}
 	/>
 	<Input name="thumbnail" type="file" label="Project Thumbnail" />
 
 	<button
 		class="w-full py-2 px-4 border bg-blue-700 rounded text-blue-50 hover:bg-blue-600 transition duration-300"
-		>Add Project</button
+		disabled={loading}>{loading ? "Loading..." : "Add Project"}</button
 	>
 </form>
